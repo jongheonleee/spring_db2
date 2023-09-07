0df6badf81af9d62dcf26ac5ba1e7bb18138f641
jongheonleee <qwefghnm1212@gmail.com>
2023-09-07 14:36:51 +0900

    JPA 1

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,21 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    // JPA는 기본 생성자 필수임
     public Item() {
     }
 
